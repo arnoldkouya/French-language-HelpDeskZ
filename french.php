09e0a021817b3d186caab783403731193ea7bfc7
gilles94 <gilleskonannguessan@gmail.com>
2018-08-22 18:19:10 +0000

    Adding French language for HelpDeskZ :)

---
+++ french.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * @package HelpDeskZ
+ * @website: http://www.helpdeskz.com
+ * @community: http://community.helpdeskz.com
+ * @author Welly Arnold KOUYA - arnoldkouya@gmail.com
+ * @link https://github.com/arnoldkouya/French-language-HelpDeskZ
+ * @phone +225 49 11 95 98 / @skype : arnoldkouya
+ * @since 1.0.0
+ */
+$LANG['HOME'] = 'Accueil';
+$LANG['MY_TICKETS'] = 'Mes Tickets';
+$LANG['SUBMIT_A_TICKET'] = 'Soumettre un Ticket';
+$LANG['KNOWLEDGEBASE'] = 'Base de connaissances';
+$LANG['NEWS'] = 'Actualités';
+$LANG['ACCOUNT_LOGIN'] = 'Connexion au compte';
+$LANG['LASTEST_UPDATES'] = 'Dernières mises à jour';
+$LANG['YOUR_EMAIL_ADDRESS'] = 'Votre adresse email';
+$LANG['YOUR_PASSWORD'] = 'Votre mot de passe';
+$LANG['REMEMBER_ME'] = 'Se souvenir de moi';
+$LANG['LOST_PASSWORD'] = 'Mot de passe oublié';
+$LANG['LOGIN'] = 'S\'identifier';
+$LANG['ACCOUNT'] = 'Compte';
+$LANG['MY_PROFILE'] = 'Mon profil';
+$LANG['PREFERENCES'] = 'Préférences';
+$LANG['CHANGE_PASSWORD'] = 'Changer de mot de passe';
+$LANG['LOGOUT'] = 'Se déconnecter';
+$LANG['VIEW_ALL_NEWS'] = 'Voir toutes les nouvelles';
+$LANG['POSTED_BY_ON'] = 'Posté par %author% le %date%';
+$LANG['OLDEST_POSTS'] = 'Anciennes publications';
+$LANG['NEWEST_POSTS'] = 'Nouvelles publications';
+$LANG['MOST_POPULAR_ARTICLES'] = 'Articles les plus populaires';
+$LANG['NEWEST_ARTICLES'] = 'Nouveaux articles';
+$LANG['MORE_TOPICS'] = 'Plus de sujets';
+$LANG['FILE_NOT_FOUND'] = 'Fichier non trouvé.';
+$LANG['SEARCH'] = 'Rechercher';
+$LANG['SEARCH_IN_KNOWLEDGEBASE'] = 'Rechercher dans la base de connaissance';
+$LANG['SEARCH_RESULTS'] = 'Resultats de la recherche';
+$LANG['SEARCH_NOT_FOUND'] = 'Nous n\'avons pas été en mesure de trouver quelque chose de pertinent pour votre recherche';
+$LANG['ENTER_EMAIL_RECOVER_PASSWORD'] = 'Veuillez entrer votre adresse e-mail enregistrée et cliquez sur Soumettre. Vous recevrez un email avec un lien pour réinitialiser votre mot de passe sous peu.';
+$LANG['ACCOUNT_INFORMATION'] = 'Information sur le compte';
+$LANG['EMAIL'] = 'E-mail';
+$LANG['CAPTCHA_VERIFICATION'] = 'Vérification Captcha';
+$LANG['ENTER_TEXT_CAPTCHA'] = 'Veuillez entrer le texte que vous voyez dans l\'image dans la zone de texte ci-dessous (nous l\'utilisons pour empêcher les soumissions automatisées).';
+$LANG['SUBMIT'] = 'Soumettre';
+$LANG['CANT_FIND_SOLUTION_SUBMIT_TICKET'] = 'Si vous ne trouvez pas de solution à votre problème, vous pouvez soumettre un ticket en sélectionnant le service approprié ci-dessous.';
+$LANG['SELECT_DEPARTMENT_TICKET'] = 'S\'il vous plaît, sélectionnez un département où votre billet sera soumis';
+$LANG['CSRF_ERROR'] = 'Nous avons rencontré un problème (falsification de requête intersite détectée); Veuillez réessayer.';
+$LANG['DEPARTMENTS'] = 'Départements';
+$LANG['NEXT'] = 'Suivant';
+$LANG['INVALID_EMAIL_OR_PASSWORD'] = 'Adresse e-mail ou mot de passe invalide.';
+$LANG['YOUR_TICKET_DETAILS'] = 'Les détails de votre ticket';
+$LANG['ENTER_TICKET_DETAILS'] = 'Entrez les détails de votre billet ci-dessous. Si vous signalez un problème, n\'oubliez pas de fournir autant d\'informations pertinentes que possible.';
+$LANG['GENERAL_INFORMATION'] = 'Informations générales';
+$LANG['FULL_NAME'] = 'Nom complet';
+$LANG['PRIORITY'] = 'Priorité';
+$LANG['YOUR_MESSAGE'] = 'Votre message';
+$LANG['SUBJECT'] = 'sujet';
+$LANG['ATTACHMENTS'] = 'Pièces jointes';
+$LANG['INVALID_CAPTCHA_CODE'] = 'ERREUR: Code image de vérification non valide';
+$LANG['INVALID_EMAIL_ADDRESS'] = 'ERREUR: veuillez spécifier une adresse e-mail valide';
+$LANG['ONE_REQUIRED_FIELD_EMPTY'] = 'Un des champ (s) requis est vide';
+$LANG['ERROR_UPLOADING_A_FILE'] = 'Une erreur s\'est produite lors du téléchargement d\'un fichier.';
+$LANG['INVALID_FILE_EXTENSION'] = 'Extension de fichier invalide.';
+$LANG['FILE_NOT_ALLOWED'] = 'Le fichier n\'est pas autorisé.';
+$LANG['FILE_IS_BIG'] = 'Le fichier est trop gros. La taille maximale est %size%';
+$LANG['REQUEST_RECEIVED'] = 'Votre demande a été reçue';
+$LANG['REQUEST_RECEIVED_RESPONSE'] = 'Nous avons reçu votre demande et notre équipe vous répondra dans les plus brefs délais. Vous pouvez vous connecter au helpdesk pour vérifier l\'état de votre demande ou consulter votre courrier électronique pour obtenir des mises à jour supplémentaires..';
+$LANG['TICKET_ID'] = 'ID du billet';
+
+$LANG['WE_SENT_YOUR_NEW_PASSWORD'] = 'Nous avons envoyé un email avec un nouveau mot de passe à l\'adresse fournie';
+$LANG['EMAIL_WAS_NOT_FOUND'] = 'Email introuvable.';
+$LANG['SEARCH_TICKET_ID'] = 'ID du ticket de recherche';
+$LANG['VIEW_TICKETS'] = 'Voir les tickets';
+$LANG['VIEW_TICKETS_DESCRIPTION'] = 'Vous trouverez ci-dessous les billets que vous avez soumis par le passé. Cliquez sur l\'objet d\'un ticket pour voir le ticket et son historique.';
+$LANG['LAST_UPDATE'] = 'Dernière mise à jour';
+$LANG['DEPARTMENT'] = 'Département';
+$LANG['STATUS'] = 'Status';
+$LANG['VIEW_TICKET_ID'] = 'Voir le ticket #%id%';
+$LANG['CREATED'] = 'Créé';
+$LANG['UPDATED'] = 'Actualisé';
+$LANG['TICKET_IS_CLOSED'] = 'Le ticket est actuellement fermé.';
+$LANG['ADD_REPLY'] = 'Ajouter une réponse';
+$LANG['YOUR_REPLY_SENT'] = 'Votre réponse à ce ticket a été soumise avec succès';
+$LANG['USER'] = 'Utilisateur';
+$LANG['STAFF'] = 'Personnel';
+$LANG['POSTED_ON'] = 'Posté sur';
+$LANG['TICKET_NOT_FOUND_OR_PERMISSION'] = 'Désolé, nous n\'avons pas pu trouver le ticket que vous avez demandé ou vous n\'avez pas la permission de le voir.';
+$LANG['TICKET_NOT_FOUND'] = 'Ticket non trouvé.';
+$LANG['YOUR_PROFILE_UPDATED'] = 'Votre profil a été mis a jour.';
+$LANG['UPDATE'] = 'Mettre à jour';
+$LANG['YOUR_PREFERENCES_SAVED'] = 'Vos préférences ont été enregistrées.';
+$LANG['GENERAL_OPTIONS'] = 'Options générales';
+$LANG['TIMEZONE'] = 'Fuseau horaire';
+$LANG['DEFAULT_TIMEZONE'] = 'Fuseau horaire par défaut';
+$LANG['PASSWORD_UPDATED'] = 'Mot de passe mis à jour.';
+$LANG['PASSWORD_DETAILS'] = 'Détails du mot de passe';
+$LANG['EXISTING_PASSWORD'] = 'Mot de passe existant';
+$LANG['NEW_PASSWORD'] = 'Nouveau mot de passe';
+$LANG['CONFIRM'] = 'Confirmer';
+$LANG['EXISTING_PASSWORD_INCORRECT'] = 'Votre mot de passe existant est incorrect.';
+$LANG['NEW_PASSWORDS_DO_NOT_MATCH'] = 'Les nouveaux mots de passe entrés ne correspondent pas.';
+$LANG['EMAIL_ASSOCIATED_OTHER_ACCOUNT'] = 'L\'adresse email est déjà associée à un autre compte.';
+$LANG['WE_ARE_PERFORMING_MAINTENANCE'] = 'Nous effectuons actuellement la maintenance programmée. Toutes nos excuses pour le dérangement causé';
+$LANG['MAINTENANCE_MODE'] = 'Mode de Maintenance';
+$LANG['PAGE_NOT_FOUND'] = 'Page introuvable - 404';
+$LANG['CLICK_TO_BACK_HELPDESK'] = 'Cliquez pour revenir à la page d\'accueil du helpdesk';
+$LANG['OPEN'] = 'Ouvert';
+$LANG['AWAITING_REPLY'] = 'En attente de réponse';
+$LANG['IN_PROGRESS'] = 'En cours';
+$LANG['ANSWERED'] = 'Répondu';
+$LANG['CLOSED'] = 'Fermé';
+?>
